fffb66c4af8a3403aae7fb711e3ebdf59a0a4cd7
Gusjarki <desm.g@yandex.ru>
2022-06-27 22:10:09 +0300

    Добавьте файлы проекта.

---
+++ OddorEven/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+public class Kata
+{
+    public static string OddOrEven(int[] array)
+    {
+        int result = 0;
+        foreach (var item in array)
+        {
+            result += item;
+        }
+        if (result % 2 == 0 || result == 0)
+        {
+            return "even";
+        }
+        return "odd";
+    }
+}
